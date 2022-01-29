df0e32c3f26b5f300b0f6cd6517f27d64e0fee11
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2022-01-29 15:05:25 +0100

    Added Hover mechanic to HelicopterController

--- Team 7/Assets/_Scripts/Benjamin/HelicopterContollerBT.cs
+++ Team 7/Assets/_Scripts/Benjamin/HelicopterContollerBT.cs
@@ -3,11 +3,29 @@ using UnityEngine;
 public class HelicopterContollerBT : MonoBehaviour {
 
 	public new Rigidbody rigidbody;
+	private bool isHovering;
 	[SerializeField] private float enginePower;
 	[SerializeField] private float rotationForce;
 	private void Update() {
-		rigidbody.AddForce(transform.up * enginePower * Mathf.Clamp(Input.GetAxis("Vertical") * Time.deltaTime, 0, 1.5f));
-		Debug.Log(transform.up * enginePower * Input.GetAxis("Vertical") * Time.deltaTime);
+		VerticalMovement();
+		Rotation();
+
+		if (Input.GetKeyDown(KeyCode.Space) && !isHovering) {
+			rigidbody.velocity = Vector3.zero;
+			rigidbody.useGravity = false;
+			isHovering = true;
+		}
+		else if(Input.GetKeyDown(KeyCode.Space) && isHovering) {
+			rigidbody.useGravity = true;
+			isHovering = false;
+		}
+	}
+	private void Rotation() {
 		transform.Rotate(Vector3.back * rotationForce * Input.GetAxis("Horizontal") * Time.deltaTime);
+	}
+	private void VerticalMovement() {
+		if (!isHovering) {
+			rigidbody.AddForce(transform.up * enginePower * Mathf.Clamp(Input.GetAxis("Vertical") * Time.deltaTime, 0, 1.5f));
+		}
 	}
 }
