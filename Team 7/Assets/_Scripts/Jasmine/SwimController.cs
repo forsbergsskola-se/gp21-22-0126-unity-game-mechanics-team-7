7802651cf5d44a8443d504235cb43d028939796d
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2022-01-31 18:05:25 +0100

    Pseudo code/thoughts added on swimcontroller

--- Team 7/Assets/_Scripts/Jasmine/SwimController.cs
+++ Team 7/Assets/_Scripts/Jasmine/SwimController.cs
@@ -5,10 +5,25 @@ using UnityEngine;
 public class SwimController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //PSEUDO CODE
+    /*
+    If in water disable other controllers
+    Turn player so he is horizontal
+    W = up in water
+    A = left
+    S = down in water
+    D = right
+    
+    Changing where penguin is facing when switching direction
+    Would like to add slight rotation to player on up and then to get a nicer swim
+    Maybe need to disable locked rotation??
+    Should water detection be in it's own script?
+    
+    Add force to then counteract the force from underwater currents
+    Need to be bigger than underwater current so you can get out by swimming
+    
+    Can't walk on seafloor for now
+    */
 
     // Update is called once per frame
     void Update()
