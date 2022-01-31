8fae0a47da375d2dcbcc0bb488c3c025a8ba7dd8
Partisan Programmer <oliver.kallerfors@student.forsbergsskola.se>
2022-01-31 19:24:11 +0100

    Added Commands for teleportation

--- Team 7/Assets/_Scripts/FromClass/CommandContainer.cs
+++ Team 7/Assets/_Scripts/FromClass/CommandContainer.cs
@@ -3,11 +3,18 @@ using UnityEngine;
 // Contains generic commands for both player AND NPCs
 public class CommandContainer : MonoBehaviour {
 
+	//Moving
 	public float walkCommand;
+	//Flying
 	public float flyCommand;
 	public float flyRotateCommand;
+	//Jumping
 	public bool jumpCommand;
 	public bool jumpCommandDown;
 	public bool jumpCommandUp;
+	//Hovering
 	public bool hoverCommand;
+	//Teleporting
+	public bool teleportBehindTargetCommand;
+	public bool chargedTeleportCommand;
 }
