c0e38e32c673b5e1100f2b2fb7b84fd819104518
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2022-02-01 23:36:28 +0100

    Swimming mechanic added

--- Team 7/Assets/_Scripts/Jasmine/SwimController.cs
+++ Team 7/Assets/_Scripts/Jasmine/SwimController.cs
@@ -4,11 +4,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SwimController : MonoBehaviour {
+    [SerializeField] private float swimHorizontalSpeed;
+    [SerializeField] private float swimVerticalSpeed;
+
     private Vector3 _swimRotation = new(90, 90, 0);
     private Vector3 _startRotation;
 
+    private Rigidbody _rigidbody;
+    private CommandContainer commandContainer;
+    
     private void Start() {
+        _rigidbody = GetComponent<Rigidbody>();
+        commandContainer = GetComponentInChildren<CommandContainer>();
+
         RotateToSwimRotation();
+    }
+
+    private void FixedUpdate() {
+        Swim();
+    }
+
+    private void Swim() {
+        var velocity = _rigidbody.velocity;
+        velocity = new Vector3(commandContainer.swimCommandVertical * swimVerticalSpeed, velocity.y, 0);
+        velocity = new Vector3(commandContainer.swimCommandHorizontal * swimHorizontalSpeed, velocity.x, 0);
+        _rigidbody.velocity = velocity;
     }
 
     private void RotateToSwimRotation() {
