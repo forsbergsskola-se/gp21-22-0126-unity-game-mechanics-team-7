b3973cbe9e28036c987278b3340f9e966cbdddea
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2022-02-01 23:36:11 +0100

    Swim commands added to command container and player input controller

--- Team 7/Assets/_Scripts/FromClass/CommandContainer.cs
+++ Team 7/Assets/_Scripts/FromClass/CommandContainer.cs
@@ -5,17 +5,25 @@ public class CommandContainer : MonoBehaviour {
 
 	//Moving
 	public float walkCommand;
+	
 	//Flying
 	public float flyCommand;
 	public float flyRotateCommand;
+	
 	//Jumping
 	public bool jumpCommand;
 	public bool jumpCommandDown;
 	public bool jumpCommandUp;
+	
 	//Hovering
 	public bool hoverCommand;
+	
 	//Teleporting
 	public bool teleportBehindTargetCommand;
 	public bool chargedTeleportCommand;
 	public bool chargedTeleportUpCommand;
+	
+	//Swimming
+	public float swimCommandHorizontal;
+	public float swimCommandVertical;
 }
--- Team 7/Assets/_Scripts/FromClass/PlayerInputController.cs
+++ Team 7/Assets/_Scripts/FromClass/PlayerInputController.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 
 public class PlayerInputController : MonoBehaviour {
 	private CommandContainer commandContainer;
-	private float walkInput;
 	private float flyInput;
 	private float flyRotateInput;
+	private float swimVerticalInput;
+	private float swimHorizontalInput;
+	private float walkInput;
+	
 	private bool jumpInputDown;
 	private bool jumpInputUp;
 	private bool jumpInput;
@@ -24,34 +27,48 @@ public class PlayerInputController : MonoBehaviour {
 	private void GetInput() {
 		//Movement
 		walkInput = Input.GetAxis("Horizontal");
+		
 		//Jumping
 		jumpInputDown = Input.GetButtonDown("Jump");
 		jumpInputUp = Input.GetButtonUp("Jump");
 		jumpInput = Input.GetButton("Jump");
+		
 		//Flying
 		flyInput = Input.GetAxis("Vertical");
 		flyRotateInput = Input.GetAxis("Horizontal");
 		hoverInput = Input.GetButtonDown("Jump");
+		
 		//Teleportation
 		teleportBehindTargetInput = Input.GetKey(KeyCode.E);
 		chargedTeleportInput = Input.GetKey(KeyCode.LeftShift);
 		chargedTeleportUpInput = Input.GetKeyUp(KeyCode.LeftShift);
+		
+		//Swimming
+		swimHorizontalInput = Input.GetAxis("Horizontal");
+		swimVerticalInput = Input.GetAxis("Vertical");
 	}
 
 	private void SetCommands() {
 		//Movement
 		commandContainer.walkCommand = walkInput;
+		
 		//Jumpingc
 		commandContainer.jumpCommandDown = jumpInputDown;
 		commandContainer.jumpCommandUp = jumpInputUp;
 		commandContainer.jumpCommand = jumpInput;
+		
 		//Flying
 		commandContainer.flyCommand = flyInput;
 		commandContainer.flyRotateCommand = flyRotateInput;
 		commandContainer.hoverCommand = hoverInput;
+		
 		//Teleportation
 		commandContainer.teleportBehindTargetCommand = teleportBehindTargetInput;
 		commandContainer.chargedTeleportCommand = chargedTeleportInput;
 		commandContainer.chargedTeleportUpCommand = chargedTeleportUpInput;
+		
+		//Swimming
+		commandContainer.swimCommandHorizontal = swimHorizontalInput;
+		commandContainer.swimCommandVertical = swimVerticalInput;
 	}
 }
