4eecf08d6374509780ad51ee13093205f6ff292c
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2022-02-01 22:35:31 +0100

    Penguin rotates when swimming is enabled

--- Team 7/Assets/_Scripts/Jasmine/SwimController.cs
+++ Team 7/Assets/_Scripts/Jasmine/SwimController.cs
@@ -1,33 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwimController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    //PSEUDO CODE
-    /*
-    If in water disable other controllers
-    Turn player so he is horizontal
-    W = up in water
-    A = left
-    S = down in water
-    D = right
-    
-    Changing where penguin is facing when switching direction
-    Would like to add slight rotation to player on up and then to get a nicer swim
-    Maybe need to disable locked rotation??
-    Should water detection be in it's own script?
-    
-    Add force to then counteract the force from underwater currents
-    Need to be bigger than underwater current so you can get out by swimming
-    
-    Can't walk on seafloor for now
-    */
+public class SwimController : MonoBehaviour {
+    private Vector3 _swimRotation = new(90, 90, 0);
+    private Vector3 _startRotation;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Start() {
+        RotateToSwimRotation();
+    }
+
+    private void RotateToSwimRotation() {
+        _startRotation = gameObject.transform.eulerAngles;
+        gameObject.transform.eulerAngles = Vector3.Lerp(_startRotation, _swimRotation, Time.time);
     }
 }
