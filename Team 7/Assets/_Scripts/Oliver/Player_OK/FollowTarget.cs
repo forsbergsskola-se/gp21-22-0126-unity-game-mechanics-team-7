5775a2fe191cbda0989e6ab0b00d1490431204c4
Partisan Programmer <oliver.kallerfors@student.forsbergsskola.se>
2022-02-01 17:59:11 +0100

    Added Position Updater and position SO

---
+++ Team 7/Assets/_Scripts/Oliver/Player_OK/FollowTarget.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour{
+    [SerializeField] PositionSO targetPosition;
+
+
+    void Update(){
+    }
+}
