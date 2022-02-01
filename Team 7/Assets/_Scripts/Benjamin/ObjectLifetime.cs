1afc0c8caf905029d1c538cb43115404f04c9e17
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2022-02-01 18:51:20 +0100

    Changed bombs to also be destroyed on hitting ground.

--- Team 7/Assets/_Scripts/Benjamin/ObjectLifetime.cs
+++ Team 7/Assets/_Scripts/Benjamin/ObjectLifetime.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,5 +15,11 @@ public class ObjectLifetime : MonoBehaviour {
         elapsedTime += Time.deltaTime;
 
         if (lifeTime < elapsedTime) Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if (other.gameObject.CompareTag("Ground")) {
+            Destroy(gameObject);
+        }
     }
 }
