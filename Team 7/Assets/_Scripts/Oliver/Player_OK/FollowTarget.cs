9db7ac57750564087d15ec5e075dd4bbf986a846
Partisan Programmer <oliver.kallerfors@student.forsbergsskola.se>
2022-02-03 14:36:43 +0100

    Started working on AI Pathfinding

--- Team 7/Assets/_Scripts/Oliver/Player_OK/FollowTarget.cs
+++ Team 7/Assets/_Scripts/Oliver/Player_OK/FollowTarget.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class FollowTarget : MonoBehaviour{
     [SerializeField] PositionSO targetPosition;
+    [SerializeField] PortalSO portalOne;
+    [SerializeField] PortalSO portalTwo;
     [SerializeField] float aggroRange;
     [SerializeField] float moveSpeed;
 
@@ -13,6 +15,36 @@ public class FollowTarget : MonoBehaviour{
         var distanceToTarget=Vector3.Distance(this.transform.position, targetPosition.currentPosition);
 
         var directionToTarget =  targetPosition.currentPosition.x- transform.position.x;
+
+        var distanceToPortalOne =  portalOne.portalPosition.currentPosition - transform.position;
+        var distanceFromPortalOneToTarget = targetPosition.currentPosition - portalOne.portalPosition.currentPosition;
+        var portalOnePath = distanceToPortalOne + distanceFromPortalOneToTarget;
+        
+        var distanceToPortalTwo =  portalTwo.portalPosition.currentPosition - transform.position;
+        var distanceFromPortalTwoToTarget = targetPosition.currentPosition - portalTwo.portalPosition.currentPosition;
+        var portalTwoPath = distanceToPortalTwo + distanceFromPortalTwoToTarget;
+        
+        var shortestPath = Mathf.Min(distanceToTarget, Mathf.Abs(portalOnePath.magnitude), Mathf.Abs(portalTwoPath.magnitude));
+       
+        
+        
+        Debug.Log("Shortest path:" + shortestPath);
+
+        Vector3 bestPath = transform.position;
+        if (distanceToTarget <= shortestPath){
+             Debug.Log("Standard distance" + distanceToTarget);
+            bestPath = targetPosition.currentPosition;
+        }
+        else if (Mathf.Abs(portalOnePath.magnitude) <= shortestPath){
+            Debug.Log("portalOnePath" + portalOnePath.magnitude);
+            bestPath = portalOnePath;
+        }
+        else if (Mathf.Abs(portalTwoPath.magnitude) <= shortestPath){
+            Debug.Log("portalTwoPath" + portalTwoPath.magnitude);
+            bestPath = portalTwoPath;
+        }
+        
+        
         
         if (directionToTarget > 0 ){
             transform.rotation = new Quaternion(0, 0, 0, 0);
@@ -20,9 +52,9 @@ public class FollowTarget : MonoBehaviour{
         else if (directionToTarget < 0){
             transform.rotation = new Quaternion(0, 180, 0, 0);
         }
-        Debug.Log(distanceToTarget);
-        if (distanceToTarget < aggroRange){
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition.currentPosition, moveSpeed*Time.deltaTime);
+        if (bestPath.magnitude < aggroRange){
+            
+            transform.position = Vector3.MoveTowards(transform.position, bestPath, moveSpeed*Time.deltaTime);
         }
         
         
