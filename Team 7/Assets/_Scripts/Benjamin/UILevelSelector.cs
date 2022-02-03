50951b5a479641baaa073badaffbadf49f599065
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2022-02-03 13:02:34 +0100

    Add Scene Change UI

---
+++ Team 7/Assets/_Scripts/Benjamin/UILevelSelector.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UILevelSelector : MonoBehaviour
+{
+	public void LoadScene1() {
+		SceneManager.LoadScene(0);
+	}
+	
+	public void LoadScene2() {
+		SceneManager.LoadScene(1);
+	}
+}
