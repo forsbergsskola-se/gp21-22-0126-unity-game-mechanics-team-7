a28430af0b6fee3209b92c421e6dbf0b0ff2a526
Partisan Programmer <oliver.kallerfors@student.forsbergsskola.se>
2022-02-03 15:32:59 +0100

    Added remove portal gun on scene change

--- Team 7/Assets/_Scripts/Oliver/ChangeToNextScene.cs
+++ Team 7/Assets/_Scripts/Oliver/ChangeToNextScene.cs
@@ -4,13 +4,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ChangeToNextScene : MonoBehaviour
-{
+public class ChangeToNextScene : MonoBehaviour{
+
+    [SerializeField] PortalGun portalGun;
+
+
+    void Awake(){
+        portalGun = FindObjectOfType<PortalGun>();
+    }
+
     void OnTriggerEnter(Collider other){
         if (other.CompareTag("Player")){
             Debug.Log("Loading next scene");
             int currentScene = SceneManager.GetActiveScene().buildIndex;
             SceneManager.LoadScene(currentScene + 1);
+            portalGun.portalGunIsActive = false;
+
         }
         
     }
