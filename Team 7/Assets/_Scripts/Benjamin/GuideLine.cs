29879a614e2d486eef84921c433b1d5b3df03607
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2022-02-03 10:37:03 +0100

    Add Door & Guide Lines

---
+++ Team 7/Assets/_Scripts/Benjamin/GuideLine.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuideLine : MonoBehaviour
+{
+	[SerializeField] private GameObject button;
+	[SerializeField] private GameObject color;
+	private void Update() {
+		if (button.gameObject.GetComponent<ButtonTrigger>().buttonTriggered) {
+			color.gameObject.SetActive(false);
+		}
+		else {
+			color.gameObject.SetActive(true);
+		}
+	}
+}
