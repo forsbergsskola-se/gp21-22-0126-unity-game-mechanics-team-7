06cd68fc4ae0132a3079442e12026ac179cb889d
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2022-02-08 00:15:22 +0100

    Normalize Volume

--- Team 7/Assets/_Scripts/Benjamin/RestartLevelDeath.cs
+++ Team 7/Assets/_Scripts/Benjamin/RestartLevelDeath.cs
@@ -5,7 +5,6 @@ public class RestartLevelDeath : MonoBehaviour
 {
 	private void OnTriggerEnter(Collider other) {
 		if (other.gameObject.CompareTag("Player")) {
-			FMODUnity.RuntimeManager.GetBus("Bus:/").setVolume(0);
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 		}
 	}
--- Team 7/Assets/_Scripts/Oliver/QuitGame.cs
+++ Team 7/Assets/_Scripts/Oliver/QuitGame.cs
@@ -1,10 +1,7 @@
 using UnityEngine;
 
 public class QuitGame : MonoBehaviour{
-    private void Awake() {
-        FMODUnity.RuntimeManager.GetBus("Bus:/").setVolume(1);
-    }
-
+    
     public void QuitApplication(){
         Application.Quit();
     }
