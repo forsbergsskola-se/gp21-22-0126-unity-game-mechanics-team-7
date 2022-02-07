08a8f34487b3c3c54d450fd024f7e441d708499e
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2022-02-07 19:15:51 +0100

    Shark swim patrolling works

--- Team 7/Assets/_Scripts/Jasmine/SharkSwim.cs
+++ Team 7/Assets/_Scripts/Jasmine/SharkSwim.cs
@@ -8,42 +8,79 @@ using Random = System.Random;
 public class SharkSwim : MonoBehaviour {
     [SerializeField] private float swimHorizontalSpeed;
     [SerializeField] private float swimVerticalSpeed;
-    [SerializeField] private PositionSO[] patrollingPoints;
+    [SerializeField] private PositionSO[] patrolPoints;
+    
     private Rigidbody _rigidbody;
     private Vector3 _targetPosition;
-    private Random _random = new Random();
-    private bool _targetPosReached;
-
+    
+    private int destPointIndex = 0;
+    private float pointReachDistance = 3;
+    private bool pointReached;
+    
     private void Start() {
+        _targetPosition = patrolPoints[0].currentPosition;
         _rigidbody = GetComponent<Rigidbody>();
-        _targetPosition = RandomizeNextPoint();
     }
-    
-    private void Update() {
-        Swim();
-    }
-    
-    private void Swim() {
-        Vector3 currentPosition = transform.position;
 
-        if (currentPosition == _targetPosition) {
-            _targetPosition = RandomizeNextPoint();
+    private void Update() {
+        SwimPatrol();
+    }
+
+    private void SwimPatrol() {
+        DecidePoint();
+        SwimToPoint();
+    }
+
+    private void DecidePoint() {
+        var distanceToPoint = Vector3.Distance(transform.position, _targetPosition);
+
+        if (distanceToPoint < pointReachDistance) {
+            pointReached = true;
+        }
+        
+        if (pointReached) {
+            SwimToNextPatrolPoint();
         }
 
-        Vector3.Lerp(currentPosition, _targetPosition, swimHorizontalSpeed);
+        pointReached = false;
+    }
 
-        /*float verticalDifference = currentPosition.y - _targetPosition.y;
-        float horizontalDifference = currentPosition.x - _targetPosition.x;
+    private void SwimToPoint() {
+        Vector3 vectorDistance = _targetPosition - transform.position;
+        Debug.Log(vectorDistance);
 
+        if (vectorDistance.x <= 0) {
+            _rigidbody.velocity = new Vector3(_targetPosition.x * -swimHorizontalSpeed, _rigidbody.velocity.y, 0);
+
+        } else{
+            _rigidbody.velocity = new Vector3(_targetPosition.x * swimHorizontalSpeed, _rigidbody.velocity.y, 0);
+        }
+    }
+
+    private void SwimToNextPatrolPoint() {
+        // Returns if no points have been set up
+        if (patrolPoints.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        _targetPosition = patrolPoints[destPointIndex].currentPosition;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPointIndex = (destPointIndex + 1) % patrolPoints.Length;
+    }
+
+    /*private void Start() {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        SwimPatrol();
+    }
+
+    private void SwimPatrol() {
         var velocity = _rigidbody.velocity;
-        velocity = new Vector3(verticalDifference * swimVerticalSpeed, velocity.y, 0);
-        velocity = new Vector3(horizontalDifference * swimHorizontalSpeed, velocity.x, 0);
-        
-        _rigidbody.velocity = velocity;*/
-    }
-
-    private Vector3 RandomizeNextPoint() {
-        int randomPoint = _random.Next(0, patrollingPoints.Length);
-        return patrollingPoints[randomPoint].currentPosition;
-    }
+        var targetPoint = RandomizeNextPoint();
+        Vector3.Lerp(transform.position, targetPoint, 5);
+    }*/
 }
