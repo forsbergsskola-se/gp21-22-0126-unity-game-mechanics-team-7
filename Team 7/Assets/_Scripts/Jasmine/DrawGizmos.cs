76f71e87e65e44977b244e04f6aa06e67df537bc
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2022-02-07 19:15:35 +0100

    DrawGizmos script for shark patrolling points

---
+++ Team 7/Assets/_Scripts/Jasmine/DrawGizmos.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawGizmos : MonoBehaviour
+{
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, 1);
+    }
+}
