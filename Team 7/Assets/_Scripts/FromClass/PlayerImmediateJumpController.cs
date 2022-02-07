ed07406a6ae09c3e351d93d14f459db9dc56ba41
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2022-02-07 10:32:39 +0100

    Add animation to immediate jump

--- Team 7/Assets/_Scripts/FromClass/PlayerImmediateJumpController.cs
+++ Team 7/Assets/_Scripts/FromClass/PlayerImmediateJumpController.cs
@@ -6,16 +6,19 @@ public class PlayerImmediateJumpController : MonoBehaviour {
     private GroundChecker groundChecker;
     private CommandContainer commandContainer;
     private new Rigidbody rigidbody;
+    private Animator anim;
 
     private void Start() {
         rigidbody = GetComponent<Rigidbody>();
         commandContainer = GetComponentInChildren<CommandContainer>();
         groundChecker = GetComponent<GroundChecker>();
+        anim = GetComponentInChildren<Animator>();
     }
     void Update() => HandleImmediateJump();
     private void HandleImmediateJump() {
         if (commandContainer.jumpCommandDown && groundChecker.IsGrounded) {
             rigidbody.AddForce(Vector3.up * jumpForce);
+            anim.SetBool("jump", true);
         }
     }
 }
