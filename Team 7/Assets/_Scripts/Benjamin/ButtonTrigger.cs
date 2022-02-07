75662ca5f64218e6be7d4eb511cb860cbef67e16
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2022-02-07 23:33:13 +0100

    Add Flapping sound

--- Team 7/Assets/_Scripts/Benjamin/ButtonTrigger.cs
+++ Team 7/Assets/_Scripts/Benjamin/ButtonTrigger.cs
@@ -12,12 +12,14 @@ public class ButtonTrigger : MonoBehaviour {
 		if (other.gameObject.CompareTag("Player")) {
 			button.SetActive(false);
 			buttonTriggered = true;
+			FMODUnity.RuntimeManager.PlayOneShot("event:/ITEMS/PressurePad");
 		}	
 	}
 	private void OnTriggerExit(Collider other) {
 		if (other.gameObject.CompareTag("Player")) {
 			button.SetActive(true);
 			buttonTriggered = false;
+			FMODUnity.RuntimeManager.PlayOneShot("event:/ITEMS/PressurePad");
 		}
 	}
 }
