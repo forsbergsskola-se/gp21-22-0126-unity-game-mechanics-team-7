4d04368da2a6e80ddba92873a994e91f7d6eefb3
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2022-02-07 15:58:18 +0100

    SharkSwim update not working

--- Team 7/Assets/_Scripts/Jasmine/SharkSwim.cs
+++ Team 7/Assets/_Scripts/Jasmine/SharkSwim.cs
@@ -1,21 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using Random = System.Random;
 
 public class SharkSwim : MonoBehaviour {
+    [SerializeField] private float swimHorizontalSpeed;
+    [SerializeField] private float swimVerticalSpeed;
     [SerializeField] private PositionSO[] patrollingPoints;
+    private Rigidbody _rigidbody;
+    private Vector3 _targetPosition;
+    private Random _random = new Random();
+    private bool _targetPosReached;
+
+    private void Start() {
+        _rigidbody = GetComponent<Rigidbody>();
+        _targetPosition = RandomizeNextPoint();
+    }
     
+    private void Update() {
+        Swim();
+    }
     
-    // Start is called before the first frame update
-    void Start()
-    {
+    private void Swim() {
+        Vector3 currentPosition = transform.position;
+
+        if (currentPosition == _targetPosition) {
+            _targetPosition = RandomizeNextPoint();
+        }
+
+        Vector3.Lerp(currentPosition, _targetPosition, swimHorizontalSpeed);
+
+        /*float verticalDifference = currentPosition.y - _targetPosition.y;
+        float horizontalDifference = currentPosition.x - _targetPosition.x;
+
+        var velocity = _rigidbody.velocity;
+        velocity = new Vector3(verticalDifference * swimVerticalSpeed, velocity.y, 0);
+        velocity = new Vector3(horizontalDifference * swimHorizontalSpeed, velocity.x, 0);
         
+        _rigidbody.velocity = velocity;*/
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private Vector3 RandomizeNextPoint() {
+        int randomPoint = _random.Next(0, patrollingPoints.Length);
+        return patrollingPoints[randomPoint].currentPosition;
     }
 }
