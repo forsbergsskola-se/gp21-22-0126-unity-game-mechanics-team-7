c426590fcab1782bfcd7b825ad3d07572728b177
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2022-02-08 00:12:39 +0100

    Swim code clean up

--- Team 7/Assets/_Scripts/Jasmine/Swimming/Implementation_1_Swimming/SwimController.cs
+++ Team 7/Assets/_Scripts/Jasmine/Swimming/Implementation_1_Swimming/SwimController.cs
@@ -11,13 +11,13 @@ public class SwimController : MonoBehaviour {
     [SerializeField] private Animator anim;
 
     private Rigidbody _rigidbody;
-    private CommandContainer commandContainer;
+    private CommandContainer _commandContainer;
     
     private float _currentAngle;
     
     private void Start() {
         _rigidbody = GetComponent<Rigidbody>();
-        commandContainer = GetComponentInChildren<CommandContainer>();
+        _commandContainer = GetComponentInChildren<CommandContainer>();
     }
 
     private void FixedUpdate() {
@@ -28,8 +28,8 @@ public class SwimController : MonoBehaviour {
         var velocity = _rigidbody.velocity;
         
         //Testa här om de går att ha på samma rad kod
-        velocity = new Vector3(commandContainer.swimCommandVertical * swimVerticalSpeed, velocity.y, 0);
-        velocity = new Vector3(commandContainer.swimCommandHorizontal * swimHorizontalSpeed, velocity.x, 0);
+        velocity = new Vector3(_commandContainer.swimCommandVertical * swimVerticalSpeed, velocity.y, 0);
+        velocity = new Vector3(_commandContainer.swimCommandHorizontal * swimHorizontalSpeed, velocity.x, 0);
         
         FaceSwimmingDirection();
         _rigidbody.velocity = velocity;
@@ -38,10 +38,10 @@ public class SwimController : MonoBehaviour {
     }
 
     private void FaceSwimmingDirection() {
-        float targetAngle = Mathf.Atan2(commandContainer.swimCommandVertical, commandContainer.swimCommandHorizontal) * Mathf.Rad2Deg;
+        float targetAngle = Mathf.Atan2(_commandContainer.swimCommandVertical, _commandContainer.swimCommandHorizontal) * Mathf.Rad2Deg;
         _currentAngle = Mathf.LerpAngle(targetAngle, _currentAngle, Time.deltaTime);
 
-        if (commandContainer.swimCommandHorizontal != 0 || commandContainer.swimCommandVertical != 0) {
+        if (_commandContainer.swimCommandHorizontal != 0 || _commandContainer.swimCommandVertical != 0) {
             transform.rotation = Quaternion.Euler(-_currentAngle + 90, 90, 0);
         }
     }
