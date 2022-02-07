ac5822af930bb12f9d4cce05755ef6f01373dc11
Partisan Programmer <oliver.kallerfors@student.forsbergsskola.se>
2022-02-07 14:25:38 +0100

    Added Ghost AI

--- Team 7/Assets/_Scripts/Benjamin/EnemyAttack.cs
+++ Team 7/Assets/_Scripts/Benjamin/EnemyAttack.cs
@@ -1,8 +1,15 @@
+using System;
 using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
 	private void OnCollisionEnter(Collision other) {
+		if (other.gameObject.CompareTag("Player")) {
+			other.gameObject.GetComponent<Death>().isDead = true;
+		}
+	}
+
+	private void OnTriggerEnter(Collider other){
 		if (other.gameObject.CompareTag("Player")) {
 			other.gameObject.GetComponent<Death>().isDead = true;
 		}
--- Team 7/Assets/_Scripts/Oliver/Player_OK/FollowTarget.cs
+++ Team 7/Assets/_Scripts/Oliver/Player_OK/FollowTarget.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 
 public class FollowTarget : MonoBehaviour{
     [SerializeField] PositionSO targetPosition;
-    [SerializeField] PortalSO portalOne;
-    [SerializeField] PortalSO portalTwo;
     [SerializeField] float aggroRange;
     [SerializeField] float moveSpeed;
 
@@ -16,36 +14,6 @@ public class FollowTarget : MonoBehaviour{
 
         var directionToTarget =  targetPosition.currentPosition.x- transform.position.x;
 
-        // var distanceToPortalOne =  portalOne.portalPosition.currentPosition - transform.position;
-        // var distanceFromPortalOneToTarget = targetPosition.currentPosition - portalOne.portalPosition.currentPosition;
-        // var portalOnePath = distanceToPortalOne + distanceFromPortalOneToTarget;
-        //
-        // var distanceToPortalTwo =  portalTwo.portalPosition.currentPosition - transform.position;
-        // var distanceFromPortalTwoToTarget = targetPosition.currentPosition - portalTwo.portalPosition.currentPosition;
-        // var portalTwoPath = distanceToPortalTwo + distanceFromPortalTwoToTarget;
-        //
-        // var shortestPath = Mathf.Min(distanceToTarget, Mathf.Abs(portalOnePath.magnitude), Mathf.Abs(portalTwoPath.magnitude));
-        //
-        //
-        //
-        // Debug.Log("Shortest path:" + shortestPath);
-        //
-        // Vector3 bestPath = transform.position;
-        // if (distanceToTarget <= shortestPath){
-        //      Debug.Log("Standard distance" + distanceToTarget);
-        //     bestPath = targetPosition.currentPosition;
-        // }
-        // else if (Mathf.Abs(portalOnePath.magnitude) <= shortestPath){
-        //     Debug.Log("portalOnePath" + portalOnePath.magnitude);
-        //     bestPath = portalOnePath;
-        // }
-        // else if (Mathf.Abs(portalTwoPath.magnitude) <= shortestPath){
-        //     Debug.Log("portalTwoPath" + portalTwoPath.magnitude);
-        //     bestPath = portalTwoPath;
-        // }
-        //
-        //
-        
         if (directionToTarget > 0 ){
             transform.rotation = new Quaternion(0, 0, 0, 0);
         }
