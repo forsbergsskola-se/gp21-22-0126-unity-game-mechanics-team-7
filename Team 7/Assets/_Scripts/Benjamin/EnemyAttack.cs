82c3273cbc2a4a382ea5954aa24a068bf3579527
Jasmine Öhlin <jasmine.ohlin@student.forsbergsskola.se>
2022-02-08 19:16:20 +0100

    Null ref bug

--- Team 7/Assets/_Scripts/Benjamin/EnemyAttack.cs
+++ Team 7/Assets/_Scripts/Benjamin/EnemyAttack.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 public class EnemyAttack : MonoBehaviour
 {
 	private void OnCollisionEnter(Collision other) {
-		if (other.gameObject.CompareTag("Player")) {
+		if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("KillableNPC")) {
 			other.gameObject.GetComponent<Death>().isDead = true;
 		}
 	}
 
-	private void OnTriggerEnter(Collider other){
-		if (other.gameObject.CompareTag("Player")) {
+	private void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("KillableNPC")) {
 			other.gameObject.GetComponent<Death>().isDead = true;
 		}
 	}
