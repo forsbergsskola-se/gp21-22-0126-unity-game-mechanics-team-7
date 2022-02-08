e69aaa8802399854dca1125b1c22c8fc39602271
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2022-02-08 19:05:48 +0100

    Add KillableNPC to be... Killable

--- Team 7/Assets/_Scripts/Benjamin/RestartLevelDeath.cs
+++ Team 7/Assets/_Scripts/Benjamin/RestartLevelDeath.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 public class RestartLevelDeath : MonoBehaviour
 {
 	private void OnTriggerEnter(Collider other) {
-		if (other.gameObject.CompareTag("Player")) {
+		if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("KillableNPC")) {
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 		}
 	}
