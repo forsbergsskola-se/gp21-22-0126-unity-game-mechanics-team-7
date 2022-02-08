4126761cf8036e9aee1493e3d0e8f0d1f9971335
Benjamin Ting <benjamin.ting@student.forsbergsskola.se>
2022-02-08 19:33:28 +0100

    Killed BabyPenguin

--- Team 7/Assets/_Scripts/Benjamin/Death.cs
+++ Team 7/Assets/_Scripts/Benjamin/Death.cs
@@ -1,20 +1,23 @@
+using System;
 using UnityEngine;
 
 public class Death : MonoBehaviour {
 	
 	public bool isDead;
 	private bool isShown;
+	private Rigidbody rigidbody;
 	[SerializeField] private GameObject brokenHeart;
 	[SerializeField] private GameObject deathMessage;
+	
 	private void FixedUpdate() {
 		Kill();
 	}
 	private void Kill() {
 		if (isDead) {
-			transform.Translate(0, 2 * Time.deltaTime, -10 * Time.deltaTime);
+			transform.Translate(0, -20 * Time.deltaTime, -30 * Time.deltaTime);
 			deathMessage.SetActive(true);
 			if (!isShown) {
-				GetComponentInChildren<PlayerInputController>().enabled = false;
+				// GetComponentInChildren<CommandContainer>().enabled = false;
 				Instantiate(brokenHeart, transform.position, Quaternion.identity);
 				isShown = true;
 			}
